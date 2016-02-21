5219be797d521501a1976d2aa837545050443ea2
krocozabr <krocozabr@gmail.com>
2016-02-21 16:36:47 +0200

    cleaning tests

--- product/tests.py
+++ product/tests.py
@@ -1,11 +1,12 @@
 import json
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core.urlresolvers import reverse
-from django.test import TestCase
 from django.test.testcases import LiveServerTestCase
 from product.models import Product
 from django.test import TestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class TestProductPageWithSelenium(LiveServerTestCase):
@@ -25,9 +26,7 @@ class TestProductPageWithSelenium(LiveServerTestCase):
         """
         self.browser.get(self.live_server_url + '/products/' +
                          self.product.slug + '/')
-        # title = self.browser.find_element_by_tag_name('title')
-        # import ipdb; ipdb.set_trace()
-        # self.assertTrue(title.text() == self.product.name)
+        self.assertEqual(self.browser.title, self.product.name)
         price = self.browser.find_element_by_id('product-price')
         self.assertTrue(price.is_displayed())
         self.assertTrue(price.text == '$'+str(self.product.price))
@@ -46,7 +45,7 @@ class TestProductPageWithSelenium(LiveServerTestCase):
 
     def test_commenting(self):
         self.browser.get(self.live_server_url + '/products/' +
-                         self.product.slug+ '/')
+                         self.product.slug + '/')
         comment_user = self.browser.find_element_by_id('id_user')
         self.assertTrue(comment_user.is_displayed())
         comment_email = self.browser.find_element_by_id('id_email')
@@ -56,32 +55,15 @@ class TestProductPageWithSelenium(LiveServerTestCase):
         comment_user.send_keys("Ludvig")
         comment_email.send_keys("Ludvig@gmail.com")
         comment_text.send_keys("I like this product!")
+        self.browser.save_screenshot('screen1.png')
         self.browser.find_element_by_id("sendbutton").click()
-        # self.browser.get(self.live_server_url + 'products/' +
-        #                  self.product.slug+'/')
-        # success = self.browser.find_element_by_class_name('alert')
-        # self.assertTrue(success.is_displayed())
-        # self.assertTrue(success.text == 'Your comment added.')
-
-    def test_likes(self):
-        self.browser.get(self.live_server_url + '/login')
-        self.browser.find_element_by_id('id_username').send_keys(self.auth['username'])
-        self.browser.find_element_by_id('id_password').send_keys(self.auth['password'])
-        self.browser.find_element_by_id('submit').click()
-        self.browser.get(self.live_server_url + '/products/' + self.product.slug + '/')
-        self.assertTrue(self.browser.find_element_by_id('logout').is_displayed())
-        like = self.browser.find_element_by_id('like')
-        self.assertTrue(like.is_displayed())
-        like_count1 = self.browser.find_element_by_id('likes').text
-        like_action1 = self.browser.find_element_by_id('like').text
-        like.click()
-        # success = self.browser.find_element_by_class_name('alert')
-        # self.assertTrue(success.is_displayed())
-        # self.assertTrue(success.text == 'You liked this.')
-        # like_count2 = self.browser.find_element_by_id('likes').text
-        # like_action2 = self.browser.find_element_by_id('like').text
-        # self.assertNotEqual(like_count1, like_count2)
-        # self.assertNotEqual(like_action1, like_action2)
+        wait = WebDriverWait(self.browser, 10)
+        element = wait.until(EC.element_to_be_clickable((By.ID, "sendbutton")))
+        # self.browser.implicitly_wait(3)
+        self.browser.save_screenshot('screen2.png')
+        success = self.browser.find_element_by_class_name('alert')
+        self.assertTrue(success.is_displayed())
+        self.assertIn('Your comment added.', success.text)
 
 
 class TestMainSet(TestCase):
@@ -89,7 +71,7 @@ class TestMainSet(TestCase):
 
     def setUp(self):
         self.auth = {"username": "admin", "password": "admin"}
-        self.product = Product.objects.first()
+        self.product = Product.objects.last()
         self.product1 = Product.objects.get(pk=2)
 
     def test_show_page(self):
@@ -134,7 +116,7 @@ class TestMainSet(TestCase):
         self.assertEqual(self.client.get(reverse('product:product_view',
                          kwargs={'slug': self.product.slug})).status_code,
                          200)
-        self.assertContains(response, '<input type="button" id="like"')
+        self.assertContains(response, '<button type="submit" id="like"')
         self.client.get(reverse('logout'))
         resp = self.client.get(reverse('product:product_view',
                                            kwargs={'slug': self.product.slug}))
@@ -184,6 +166,7 @@ class TestMainSet(TestCase):
         data = dict(
             slug=self.product.slug,
         )
+        like_count = self.product.total_likes
         resp = self.client.post(reverse('product:like',
                                 kwargs={'slug': self.product.slug}),
                                 data,
@@ -194,5 +177,6 @@ class TestMainSet(TestCase):
             self.assertEqual(response.get('message'), 'You liked this')
         elif act == 'Like':
             self.assertEqual(response.get('message'), 'You disliked this')
-        else:
-            self.assertTrue(act)
+        self.assertTrue(act)
+        self.assertTrue(response.get('message', ''))
+        self.assertTrue(self.product.total_likes == like_count+1)
